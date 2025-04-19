aeb239f9e55ac1611dc2f91c0c15ca29b1d0994c
Sharath Chandra Addanapuri <118981558+Sharath-Chandra43@users.noreply.github.com>
2025-04-19 14:10:23 +0530

    Update MainContainer.js

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,11 +4,11 @@ import BooksContainer from './BooksContainer'
 
 const MainContainer = () => {
   return (
-    <div className="bg-gray-50 p-6 rounded-lg shadow-md">
+    <div className="bg-gray-50 p-6 rounded-lg shadow-md space-y-6">
         <TopBookContainer />
         <BooksContainer />
     </div>
   )
 }
 
-export default MainContainer
+export default MainContainer;
